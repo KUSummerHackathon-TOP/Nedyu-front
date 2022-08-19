6094e478975349a80c66d4ad8ebee40a4a5fdad1
yunz0926 <chldbswl9@gmail.com>
2022-08-19 23:19:43 +0900

    setting: initial setting

---
+++ src/page/Main/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { Carousel } from "@mantine/carousel";
+
+const Main = () => {
+  return (
+    <>
+      <div>Main</div>
+      <Carousel
+        withIndicators
+        //sx={{ maxWidth: 300 }}
+        slideSize="33.333333%"
+        slideGap="md"
+        loop
+        align="start"
+        mx="auto"
+        slidesToScroll={3}
+      >
+        <Carousel.Slide>
+          <ArticleItem>News 1</ArticleItem>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <ArticleItem>News 2</ArticleItem>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <ArticleItem>News 3</ArticleItem>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <ArticleItem>News 4</ArticleItem>
+        </Carousel.Slide>
+      </Carousel>
+    </>
+  );
+};
+
+export default Main;
+
+const ArticleItem = styled.div`
+  width: 200px;
+  height: 200px;
+  background: black;
+  color: white;
+`;
