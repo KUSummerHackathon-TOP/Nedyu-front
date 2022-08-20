350b19d1cdf573ca5d808f5b9b0bc9224414299e
ulysseyson <ulyseyson57@gmail.com>
2022-08-20 21:02:32 +0900

    add signin axios using Zustand

--- src/page/Signin/index.tsx
+++ src/page/Signin/index.tsx
@@ -1,14 +1,42 @@
+import axios from "axios";
 import React, { useState } from "react";
 import styled from "styled-components";
-
+import userDTStore from "../../store/userStore";
 const Login = () => {
   const [mail, setMail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const { user, setUserInfo } = userDTStore();
+
+  const submitLogin = (mail: string, password: string) => {
+    const requestBody = {
+      email: mail,
+      password: password,
+    };
+
+    axios
+      .post("/api/v1/auth/email/login", requestBody)
+      .then((res) => {
+        setUserInfo({
+          id: res.user.id,
+          email: res.user.email,
+          firstName: res.user.firstName,
+          lastName: res.user.lastName,
+          createdAt: res.user.createdAt,
+          updatedAt: res.user.updatedAt,
+          token: res.token,
+        });
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <Input value={mail} onChange={(e) => setMail(e.target.value)} />
       <Input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button>로그인</Button>
+      <Button onClick={() => submitLogin(mail, password)}>로그인</Button>
     </>
   );
 };
