5c2a94c470ce173f8ec71a4d8d8f1eae22753864
ulysseyson <ulyseyson57@gmail.com>
2022-08-21 01:32:35 +0900

    Make signin page

--- src/page/Signin/index.tsx
+++ src/page/Signin/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import userDTStore from "../../store/userStore";
 import { useNavigate } from "react-router-dom";
-const Login = () => {
+const Signin = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const { user, setUserInfo } = userDTStore();
@@ -33,24 +33,133 @@ const Login = () => {
       console.log(err);
     }
   };
-
+  const onSignup = () => {
+    navigate("/signup");
+  };
   return (
-    <>
-      <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={onSubmit}>로그인</Button>
-    </>
+    <Wrapper>
+      <Box>
+        <div className="Logo">Nedyu</div>
+        <Input
+          className="formbox"
+          value={email}
+          placeholder="이메일 주소"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className="formbox"
+          value={password}
+          placeholder="비밀번호"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button onClick={onSubmit}>
+          <div className="submit-btn">로그인</div>
+        </Button>
+        <div className="footer">
+          <div>다양한 뉴스 아티클을 확인하고 싶다면?</div>
+          <div onClick={onSignup}>회원가입</div>
+        </div>
+      </Box>
+    </Wrapper>
   );
 };
 
-export default Login;
+export default Signin;
 
+const Box = styled.div`
+  margin-top: 180px;
+  width: 358px;
+  height: 286px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .Logo {
+    margin-bottom: 41px;
+    font-family: "Montserrat";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 49px;
+    /* identical to box height */
+
+    text-align: center;
+
+    /* 6 */
+
+    color: #ffffff;
+  }
+  .footer {
+    margin-top: 10px;
+    width: 100%;
+    display: inline-flex;
+    justify-content: space-around;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 140%;
+  }
+`;
+const Wrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  top: 0;
+  background-color: #ffd751;
+  width: 100%;
+  height: 100%;
+`;
 const Input = styled.input`
-  display: block;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 13px 20px 13px 20px;
+  gap: 8px;
+  height: 46px;
   margin-bottom: 10px;
+  .formbox {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
-  width: 100px;
-  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: black;
+  color: white;
+
+  gap: 8px;
+  height: 42px;
+  margin-top: 32px;
+  .submit-btn {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 19px;
+
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 19px;
+    /* identical to box height */
+
+    /* 6 */
+
+    color: #ffffff;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
 `;
